3cf968ba4ff89beb45a1de7f406f8bac71feb731
Lukas Mueller <lam87@cornell.edu>
2013-01-12 19:55:55 -0500

    add POD and cleanup.

--- lib/Hdf5/Simple.pm
+++ lib/Hdf5/Simple.pm
@@ -1,4 +1,25 @@
 
+=head1 NAME
+
+Hdf5::Simple - a simple HDF5 interface
+
+=head1 SYNOPSYS
+
+ my $hs = Hdf5::Simple->new( { 
+    hdf5_file => $hdf5_file,
+    });
+
+ my $listref = $hs->get(
+    
+=head1 AUTHOR
+
+Lukas Mueller <lam87@cornell.edu>
+
+=head1 ACCESSORS
+
+=cut
+
+
 package Hdf5::Simple;
 
 use Moose;
@@ -8,41 +29,95 @@ use PDL::IO::HDF5;
 use File::Slurp;
 use Data::Dumper;
 
+
+=head1 hdf5_file
+
+accessor for the filename of the hdf5 file. Required when creating the object.
+
+=cut
+
 has 'hdf5_file'     => ( isa => 'Str',
 			is => 'rw',
     );
 
-has 'column_names' => ( isa => 'Ref',
-			is => 'rw',
-    );
+=head1 column_name_index
+
+accessor for a listref of column names. 
+
+Position in the array corresponds to the index in the hdf5 file.
+
+=cut
 
 has 'column_name_index' => ( isa => 'HashRef',
 			     is  => 'rw',
     );
 
+=head1 column_pos_index
+
+accessor for listref specifying the column name by position
+
+=cut
+
 has 'column_pos_index'  => ( isa => 'ArrayRef',
 			     is  => 'rw',
     );
+
+=head1 row_names
+
+accessor for hashref specifying the position by row name
+
+=cut
+
 has 'row_names'    => ( isa => 'HashRef', 
 			is  => 'rw',
     );
+
+=head1 row_name_index
+
+accessor for hashref specifying the position by row name
+
+=cut
 
 has 'row_name_index' => ( isa => 'HashRef',
 			  is  => 'rw',
     );
 
+=head1 row_pos_index
+
+accessor for an arrayref specifying the name by position (array index)
+
+=cut
+
 has 'row_pos_index'  => ( isa => 'ArrayRef',
 			  is  => 'rw',
     );
+
+=head1 hdf5
+
+accessor for the PDL::IO::HDF5 object that does the heavy lifting. Mostly used internally.
+
+=cut 
 
 has 'hdf5'         => ( isa => 'PDL::IO::HDF5',
 			is  => 'rw',
     );
 
+=head1 group
+
+accessor for the hdf5 group to be accessed. Required in the constructor.
+
+Allows to change the group during the existence of the object.
+
+=cut
+
 has 'group'        => ( isa => 'Str',
 			is => 'rw',
 			default => 'default',
     );
+
+=head1 FUNCTIONS
+
+=cut
 
 sub BUILD { 
     my $self = shift;
@@ -57,12 +132,21 @@ sub BUILD {
     }
 }
     
+=head2 index_hdf5
 
+ Usage:        $hs -> index_hdf5($file)
+ Desc:         creates the hdf5 file from file $file. File must be a 
+               tab delimited text file containing a matrix, with row and 
+               column headers.
+ Ret:          nothing
+ Args:         filename for the file to index
+ Side Effects: creates the hdf5 file
+
+=cut
 
 sub index_hdf5 {
     my $self = shift;
     my $file = shift;
-
 
     if (-e $self->hdf5_file()) { 
 	die "The file ".$self->hdf5_file()." already exists. Please delete before re-indexing.\n";
@@ -71,7 +155,6 @@ sub index_hdf5 {
     print STDERR "Converting file $file to HDF5... ";
 
     my @lines = read_file($file);
-
 
     # create a matrix containing everything
     #
@@ -92,8 +175,6 @@ sub index_hdf5 {
      	push @column_header_matrix, [ $column_headers->[$i], $i ] ;	
     }
     
-    #print STDERR Data::Dumper::Dumper(\@column_header_matrix);
-
     # create a dataset for the row headers, remove from matrix
     #
     my @row_header_matrix;
@@ -105,39 +186,35 @@ sub index_hdf5 {
     my $col_pdl  = PDL::Char->new(@column_header_matrix);
     my $row_pdl  = PDL::Char->new(@row_header_matrix);
 
-    #print STDERR $data_pdl;
-    #print STDERR $col_pdl;
-    #print STDERR $row_pdl;
-
     my $h = PDL::IO::HDF5->new(">".$self->hdf5_file());
 
     my @groups = $h->groups();
-    #print STDERR "Currently there are @groups associated with this database.\n";
 
     my $g = $h->group($self->group);
-    #print STDERR "GROUP NAME: ".$g->nameGet()."\n";
-    
 
     my $snps = $g->dataset("SNPS");
     my $cols = $g->dataset("column_names");
     my $rows = $g->dataset("row_names");
 
-    #print STDERR join "|", $snps->dims(), $cols->dims(), $rows->dims();
-
-
     $snps -> set($data_pdl);
     $cols -> set($col_pdl);
     $rows -> set($row_pdl); 
 
-
     $self->hdf5($h);
 		    
     $self->build_index_cache($self->group);
-
-
-    #print STDERR "Done.\n";
 }
 
+=head2 build_index_cache
+
+ Usage:        $hs->build_index_cache
+ Desc:         Used internally to build a cache of column and row names
+ Ret:          nothing
+ Args:
+ Side Effects:
+ Example:
+
+=cut
 
 sub build_index_cache { 
     my $self = shift;
@@ -146,18 +223,14 @@ sub build_index_cache {
     my $g = $h->group($self->group);
     my $col_dataset =  $g->dataset('column_names');
     my $cols = $col_dataset ->get();
-    # create column name hash
-    #
+
     my %col_names;
     my @col_pos;
 
     my @col_dims = $cols->dims();
     
     # not sure why there are three dims...
-#    print "ROWS, COLS, $col_dims[1], $col_dims[2]\n";
-#    print "COLS: $cols\n";
     foreach my $c (0..$col_dims[2]-1) { 
-	#print STDERR "LOOKING AT ROW $c\n";
 	my $label = $cols->atstr(0, $c);
 	my $pos   = $cols->atstr(1, $c);
 	$col_names{$label} = $pos;
@@ -170,11 +243,7 @@ sub build_index_cache {
     my $row_dataset = $g->dataset('row_names');
     my $rows = $row_dataset->get();
     
-    # create row name hash
-    #
-    #print "ROWS: ".$rows."\n";
     my @row_dims = $rows->dims();
-    #print STDERR "COL DIMS: ".(@row_dims)."\n";
     my %row_names;
     my @row_pos;
     
@@ -185,9 +254,7 @@ sub build_index_cache {
 	$row_names{$label} = $pos;
 	push @row_pos, $label;
     }
-    
-#    die "Row name index is : ".(Data::Dumper::Dumper(\%row_hash))."\n";
-    
+        
     $self->row_name_index(\%row_names);
     $self->row_pos_index(\@row_pos);
 }
@@ -201,9 +268,6 @@ sub get {
     my $p = shift;
     my $q = shift;
 
-    #print STDERR "Get: group: ". $self->group. " HDF file: ".$self->hdf5_file()."\n";
-
-    #print STDERR "\n\n\nOpening hdf file...\n";
     my $start = pdl([0, 0]);
     my $end   = pdl([3, 2]);
     #my $stride = pdl([1,1]);
@@ -212,30 +276,27 @@ sub get {
     
     my $g = $h->group($self->group);
     
-    #print STDERR "Retrieve dataset \n";
-
     my $dataset = $g->dataset('SNPS');
 
-    #print STDERR "NAME: ".$dataset->nameGet()." DIMS: ".join ", ", $dataset->dims(), "\n";
-    
-
-    #print STDERR "Retrieve data...\n";
     my $pdl = $dataset->get($start, $end); #, $stride);
-
-    
     
     return $pdl;
 }
     
+=head2 get_row
 
+ Usage:
+ Desc:         retrieve an entire row from the hdf5 file
+ Args:         a row name
+ Ret:          a hash containing the column names and corresponding values
+
+=cut
 
 sub get_row { 
     my $self= shift;
     my $row_name = shift;
 
     my $hdf = $self->hdf5();
-
-    #print STDERR Data::Dumper::Dumper($self->row_name_index());
 
     if (!defined($self->row_name_index()->{$row_name})) { 
 	print STDERR "ROW NAME $row_name DOES NOT EXIST!\n";
@@ -247,8 +308,6 @@ sub get_row {
     
     my ($cols, $rows) = $dataset->dims();
     
-    #print STDERR "$cols, $rows. row name $row_name. Row index ".$self->row_name_index()->{$row_name}."\n";
-    
     my $start = PDL->new(0, $self->row_name_index()->{$row_name});
     my $end   = PDL->new($cols-1, $self->row_name_index()->{$row_name});
     
@@ -259,19 +318,26 @@ sub get_row {
     # format the result has a hasref
     #
     my %return_data = ();
-    #print STDERR $row;
+
     my @row_dims = $row->dims();
 
-    #print Data::Dumper::Dumper(\@row_dims);
-    #print STDERR "retrieved row data: ".Data::Dumper::Dumper(\@row_data)."\n";
     my $col_pos = $self->column_pos_index();
     for (my $i=0; $i < $row_dims[1]; $i++) { 
 	$return_data{$col_pos->[$i]} = $row->atstr($i);
     }
-    #print STDERR "RETURN DATA = ". Data::Dumper::Dumper(\%return_data);
+
     return \%return_data;
     
 }
+
+=head2 get_col
+
+ Usage:
+ Desc:
+ Args:
+ Ret:
+
+=cut
 
 sub get_col { 
     my $self= shift;
@@ -291,23 +357,15 @@ sub get_col {
     
     my $col = $dataset->get($start, $end); 
     
-    #print STDERR "RETRIEVED COL: $col_name\n";
-    
- # format the result has a hasref
-    #
     my %return_data = ();
     my @col_dims = $col->dims();
-
-    #print Data::Dumper::Dumper(\@col_dims);
 
     my $row_pos = $self->row_pos_index();
     for (my $i; $i< $col_dims[2]; $i++) { 
 	$return_data{$row_pos->[$i]} = $col->atstr(0, $i);
     }
     
-    
     return \%return_data;
-    
 }
 
 

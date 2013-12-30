03a98e0196dd63c67740210365de8707dff6d7d1
Lukas Mueller <lam87@cornell.edu>
2013-12-30 18:03:51 -0500

    return page not found error in ajax format.

--- lib/Hdf5/Controller/Root.pm
+++ lib/Hdf5/Controller/Root.pm
@@ -60,7 +60,7 @@ Standard 404 error page
 
 sub default :Path {
     my ( $self, $c ) = @_;
-    $c->response->body( 'Page not found' );
+    $c->response->body( '{ error: "REST function not found." } ' );
     $c->response->status(404);
 }
 
